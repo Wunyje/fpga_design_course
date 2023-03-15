65211beed53cf7336fc4f94e1c82d8cc16d69145
Wunyje <wunyje@gmail.com>
2023-03-15 21:31:32 +0800

    creat task 2---median filter

---
+++ task_2/design_src/median_filter.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/03/15 21:26:45
+// Design Name: 
+// Module Name: median_filter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module median_filter(
+    input [7:0] dat_i,
+    input val_i,
+    output [7:0] dat_o,
+    output val_o
+    );
+endmodule
