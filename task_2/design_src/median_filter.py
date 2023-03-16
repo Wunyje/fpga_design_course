eb4e0306443bcec1aa0b01ce7545036158b19316
WT <244001428@qq.com>
2023-03-16 17:19:54 +0800

    finish software design, draw some waveform using timegen

--- task_2/design_src/median_filter.py
+++ task_2/design_src/median_filter.py
@@ -5,26 +5,57 @@ def median_filter(dat_i, width = 9):
     dat_i_r = []
     mid_arr = []
     dat_o = []
-    for i in range(1 + width//2, len(dat_i) + width//2 +1): # 
-        if i <= width: # 前width-1个元素，补零滤波
+    for i in range(1 + width//2, len(dat_i) + width//2 +1): 
+        # 对齐，确保是width个函数 
+        if i <= width: 
+            # 前width-1个元素，补零滤波
             dat_i_r = dat_i[0:i]
             dat_i_r += [0 for _ in range(width - len(dat_i_r))]
-        elif i <= len(dat_i) : # 中间width个元素，不用补零
+        elif i <= len(dat_i) : 
+            # 中间width个元素，不用补零
             dat_i_r = dat_i[i-width:i]
-        else: # 后width-1个元素，补零滤波
+        else: 
+            # 后width-1个元素，补零滤波
             dat_i_r = dat_i[i - width:len(dat_i)]
             dat_i_r += [0 for _ in range(width - len(dat_i_r))] 
-        mid_arr = bubbleSort(dat_i_r)
+        mid_arr = bubbleSort_partial_expand(dat_i_r)
         mid_val = mid_arr[width//2]
         dat_o.append(mid_val)
     return dat_o
 
+# 冒泡排序：双循环
 def bubbleSort(arr_i): 
     arr_o = arr_i
     for i in range(1, len(arr_o)): 
         for j in range(0, len(arr_o)-i): 
             if arr_o[j] > arr_o[j+1]: 
                 arr_o[j], arr_o[j + 1] = arr_o[j + 1], arr_o[j] 
+    return arr_o 
+
+# 冒泡排序：部分展开
+def bubbleSort_partial_expand(arr_i): 
+    arr_o = arr_i
+    for i in range(0, len(arr_o)): 
+        if i%2 == 0:
+            arr_o[0] = arr_o[0]
+            if arr_o[1] > arr_o[2]: 
+                arr_o[2], arr_o[1] = arr_o[1], arr_o[2] 
+            if arr_o[3] > arr_o[4]: 
+                arr_o[4], arr_o[3] = arr_o[3], arr_o[4] 
+            if arr_o[5] > arr_o[6]: 
+                arr_o[6], arr_o[5] = arr_o[5], arr_o[6] 
+            if arr_o[7] > arr_o[8]: 
+                arr_o[8], arr_o[7] = arr_o[7], arr_o[8] 
+        else:
+            arr_o[8] = arr_o[8]
+            if arr_o[0] > arr_o[1]: 
+                arr_o[1], arr_o[0] = arr_o[0], arr_o[1] 
+            if arr_o[2] > arr_o[3]: 
+                arr_o[3], arr_o[2] = arr_o[2], arr_o[3] 
+            if arr_o[4] > arr_o[5]: 
+                arr_o[5], arr_o[4] = arr_o[4], arr_o[5] 
+            if arr_o[6] > arr_o[7]: 
+                arr_o[7], arr_o[6] = arr_o[6], arr_o[7] 
     return arr_o 
 
 if __name__ == '__main__':
--- task_2/design_src/median_filter.v
+++ task_2/design_src/median_filter.v
@@ -6,11 +6,23 @@
 // Create Date: 2023/03/15 21:26:45
 // Design Name: 
 // Module Name: median_filter
-// Project Name: 
+// Project Name: xi_median_filter
 // Target Devices: 
-// Tool Versions: 
+// Tool Versions: Vivado 2019.1
 // Description: 
-// 
+//--               _   _   _   _   _   _   _   _   _   _   _   _  
+//-- clk          | |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_| |_
+//--              ____ ___ ___ ___ _______________ ___ ___ ___ ___
+//-- dat_i[7:0]   ____X___X___X___X_______________X___X___X___X___
+//--                   ___________                 _______________
+//-- val_i        ____|           |_______________|               
+//--              ________________ ___ ___ ___ _______________ ___
+//-- dat_o[7:0]   ________________X___X___X___X_______________X___
+//--                               ___________                 ___
+//-- val_o        ________________|           |_______________|   
+//
+//   delay: 8 cycle
+//
 // Dependencies: 
 // 
 // Revision:
@@ -26,5 +38,5 @@ module median_filter(
     output [7:0] dat_o,
     output      val_o
     );
-    
+
 endmodule
